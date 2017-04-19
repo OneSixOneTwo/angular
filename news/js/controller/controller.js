152e364c081b4a328028c3c9ddd95dfc913dfe41
wscats <2587575267@qq.com>
2017-04-19 17:20:18 +0800

    add news

--- news/js/controller/controller.js
+++ news/js/controller/controller.js
@@ -3,6 +3,47 @@
 (function() {
 	var controllers = angular.module("controllers", []);
 	controllers.controller("indexCtrl", ["$scope", function($scope) {
-		$scope.name = "ws"
+		$scope.name = "ws";
+		console.log("ok");
+		//默认的选项
+		$scope.item = 0;
+		$scope.itemOn = function(item) {
+			$scope.item = item;
+		}
 	}])
+	controllers.controller("recommendCtrl", function($scope, $http) {
+		$scope.title = "推荐";
+		//默认显示第一页
+		$scope.page = 0;
+		$scope.news = [];
+		$scope.loadMore = function() {
+			$http({
+				url: "http://10.3.132.40:81/news/php/index.php/news_api/show_detail_by_channel_id",
+				method: "GET",
+				params: {
+					channel_id: 6,
+					page: $scope.page++
+				}
+			}).then(function(data) {
+				$scope.news = $scope.news.concat(data.data.news_list);
+			})
+		}
+		$scope.loadMore();
+	})
+	controllers.controller("detailCtrl", function($scope,$location,$http) {
+		console.log($location.search());
+		$http({
+			url:"http://10.3.132.40:81/news/php/index.php/news_api/show_detail",
+			method: "GET",
+			params:{
+				id:$location.search().id
+			}
+		}).then(function(data){
+			console.log(data)
+			$scope.newData = data.data.news_list[0]
+		})
+	})
+	controllers.controller("entertainCtrl", function($scope) {
+		$scope.title = "娱乐"
+	})
 })();
--- news/js/directives/directives.js
+++ news/js/directives/directives.js
@@ -2,9 +2,34 @@
 ;
 (function() {
 	var directives = angular.module("directives", []);
-	directives.directive("xheader", function() {
+	//头部
+	directives.directive("wheader", function() {
 		return {
-
+			templateUrl: "directives/wheader.html"
+		}
+	})
+	//底部
+	directives.directive("wfooter", function() {
+		return {
+			templateUrl: "directives/wfooter.html"
+		}
+	})
+	//轮播图
+	directives.directive("wswiper", function() {
+		return {
+			templateUrl: "directives/wswiper.html",
+			link: function(scope, ele, attr) {
+				var swiper = new Swiper('.swiper-container', {
+					pagination: '.swiper-pagination',
+					paginationClickable: true
+				});
+			}
+		}
+	})
+	//新闻列表
+	directives.directive("wlist", function() {
+		return {
+			templateUrl: "directives/wlist.html",
 		}
 	})
 })();
